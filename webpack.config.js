5d864878869bd52c97a3c8668e7808693ecd66a4
Mitch Wilson <mitchwilson1969@gmail.com>
2016-11-25 15:35:46 -0600

    Change build and dev tasks to provide minified version for demo

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,10 @@
 var webpack = require('webpack');
-var UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
 var path = require('path');
 var env = require('yargs').argv.mode;
 
 var libraryName = 'lantern';
 
-var plugins = [], outputFile;
-
-if (env === 'build') {
-  plugins.push(new UglifyJsPlugin({ minimize: true }));
-  outputFile = libraryName + '.min.js';
-} else {
-  outputFile = libraryName + '.js';
-}
+var plugins = [], outputFile = libraryName + '.js';
 
 var config = {
   entry: __dirname + '/src/lantern.js',
